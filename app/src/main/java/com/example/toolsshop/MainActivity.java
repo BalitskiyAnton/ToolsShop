ae52f7bb6ce4e10fb468867ee876223a8a1f67a8
BalitskiyAnton <Bondarenko03130@gmail.com>
2022-05-16 12:06:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/toolsshop/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.toolsshop;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        listView = findViewById(R.id.listViewTools);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), i, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
